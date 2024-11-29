f18f5740644e071982ca2c27f4a091a364dd900d
ragul66 <ragul1.gokul2.3@gmail.com>
2024-11-29 23:41:54 +0530

    commit

---
+++ src/data/user.js
@@ -0,0 +1,87 @@
+const INFO = {
+	main: {
+		title: "Ragul_portfolio",
+		name: "Tharindu N.",
+		email: "ragulvasanth24@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/ragul66",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://www.instagram.com/ragul_vasanth66/",
+	},
+
+	homepage: {
+		title: "Full-stack web Developer 🚀",
+		description:
+			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+	},
+
+	about: {
+		title: "I’m Ragul Vasanth S M. I live in Madurai, where I design the future.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Dineease",
+			description:
+				"Dineease is a hotel seat booking platform that simplifies dining reservations. Browse menus, book your favorite seats, and enjoy a seamless dining experience—all in one place!.",
+			// logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			logo: "../image.png",
+			logo1: "../mongo.png",
+			linkText: "View Project",
+			link: "https://dineease-ten.vercel.app/",
+		},
+
+		{
+			title: "Real-time Face Detection using OpenCV in Python",
+			description:
+				"Face detection using OpenCV enables real-time identification of human faces in images, leveraging powerful computer vision algorithms for applications like security, authentication, and analytics.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			logo1: "../opencv.png",
+			linkText: "View Project",
+			link: "https://github.com/ragul66/Real-time-Face-Detection-using-OpenCV-in-Python",
+			// link: "https://medium.com/@ragulvasanth24/b570be322d25",
+		},
+
+		{
+			title: "Kannadiyar",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "../image.png",
+			logo1: "../postgresql.png",
+			linkText: "View Project",
+			link: "https://www.kannadiyar.com/",
+		},
+
+		{
+			title: "Project 4",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 5",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
